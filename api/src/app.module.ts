aef2bf81f605730bad12ec5a76c47e667703a87b
Antony <82900352+Antony-Wethenew@users.noreply.github.com>
2023-11-30 16:44:52 +0100

    add configModule

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -4,21 +4,37 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RouteApiModule } from './routes/routes.api.module';
 import { EntitiesModule } from './entities/entities.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
-console.log(__dirname  + '/../src/entities/schemas/*{.ts,.js}');
+console.log(__dirname + '/../src/entities/schemas/*{.ts,.js}');
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3307,
-      username: 'root',
-      password: 'psswd',
-      database: 'mariage',
-      entities: [__dirname + '/**/entities/schemas/*{.ts,.js}'],
-      synchronize: true,
-      logging: ['query'],
-      
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (
+        configService: ConfigService<
+          {
+            DB_USER: string;
+            DB_PASSWORD: string;
+            DB_DATABASE: string;
+            DB_HOST: string;
+            DB_PORT: number;
+          },
+          true
+        >,
+      ) => ({
+        type: 'mysql',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        entities: [__dirname + '/**/entities/schemas/*{.ts,.js}'],
+        synchronize: true,
+        logging: ['query'],
+      }),
     }),
     EntitiesModule,
     RouteApiModule,
