b42382e3230fdf5c37296caf73597bae2b809048
Antony <82900352+Antony-Wethenew@users.noreply.github.com>
2023-11-30 23:03:04 +0100

    e

--- app/app/home/page.tsx
+++ app/app/home/page.tsx
@@ -1,4 +1,7 @@
 "use client"
+
+import React from "react";
+
 export default function HomePage({children}: {children: React.ReactNode}) {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
