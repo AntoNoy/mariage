e2af0800d934cae23ee20b32feadf7ad28218df5
Antony <82900352+Antony-Wethenew@users.noreply.github.com>
2023-12-01 12:22:10 +0100

    test ftp github

--- api/script-start.sh
+++ api/script-start.sh
@@ -3,5 +3,6 @@
 # npm run stop:prod || true
 # npm run start:prod
 
-/var/services/homes/***/.npm-packages/lib/node_modules/pm2/bin/pm2 stop api-mariage
-/var/services/homes/***/.npm-packages/lib/node_modules/pm2/bin/pm2 start api-mariage
+/var/services/homes/***/.npm-packages/lib/node_modules/pm2/bin/pm2 delete api-mariage
+/var/services/homes/***/.npm-packages/lib/node_modules/pm2/bin/pm2 start dist/main.js --name api-mariage
+/var/services/homes/***/.npm-packages/lib/node_modules/pm2/bin/pm2 save
