825072300527fc001ed2483d957676bd92c9dc16
AntoNoy <anto75019@gmail.com>
2023-12-04 22:20:16 +0100

    CD raspberry

--- api/src/app.service.ts
+++ api/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! Mariage api';
+    return 'Hello World! Mariage api raspberry pm2';
   }
 }
