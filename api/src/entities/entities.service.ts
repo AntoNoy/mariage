3d2dab98e594dead5d1b9d82076d075ef8f35598
AntoNoy <anto75019@gmail.com>
2023-12-05 00:10:41 +0100

    Revert "remove argon2"
    This reverts commit db4fdbe7622528a9d17b1556399441ef923947c1.

--- api/src/entities/entities.service.ts
+++ api/src/entities/entities.service.ts
@@ -3,7 +3,9 @@ import { InjectDataSource } from "@nestjs/typeorm";
 import { Users } from "./schemas/users";
 import { DataSource } from "typeorm";
 import { Guests } from "./schemas/guests";
+import * as argon2 from 'argon2';
 import { GuestCreate, UsersCreate } from "./definitions";
+
 
 
 
@@ -14,7 +16,8 @@ export class EntitiesService {
     ) { }
 
     async createUser(payload: UsersCreate, guests: GuestCreate[]) {
-        const user = await this.dataSource.manager.save(Users, { ...payload })
+        const hashedPassword = await argon2.hash(payload.password, { type: argon2.argon2id });
+        const user = await this.dataSource.manager.save(Users, { ...payload, password: hashedPassword })
         await this.dataSource.manager.save(Guests, guests.map(guest => ({
             ...guest,
             userId: user.id,
