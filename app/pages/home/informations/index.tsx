e6451b05c5616bdb94af4403ef6c9026ce23d0ff
Antony <82900352+Antony-Wethenew@users.noreply.github.com>
2023-12-17 12:45:48 +0100

    test

--- app/pages/home/informations/index.tsx
+++ app/pages/home/informations/index.tsx
@@ -1,15 +1,43 @@
+import { Button } from "@mui/material";
+
 export default function InformationPage() {
   return (
+    // https://photo.noyeau.io/mo/request/A6doKILJP
     <div>
       <h1>InformationPage</h1>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus quam cum eius architecto recusandae minima nemo nostrum blanditiis, dicta inventore, excepturi obcaecati ipsa minus. Sit, explicabo. Incidunt dolores laborum tempora.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus quam cum eius architecto recusandae minima nemo nostrum blanditiis, dicta inventore, excepturi obcaecati ipsa minus. Sit, explicabo. Incidunt dolores laborum tempora.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus quam cum eius architecto recusandae minima nemo nostrum blanditiis, dicta inventore, excepturi obcaecati ipsa minus. Sit, explicabo. Incidunt dolores laborum tempora.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus quam cum eius architecto recusandae minima nemo nostrum blanditiis, dicta inventore, excepturi obcaecati ipsa minus. Sit, explicabo. Incidunt dolores laborum tempora.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus quam cum eius architecto recusandae minima nemo nostrum blanditiis, dicta inventore, excepturi obcaecati ipsa minus. Sit, explicabo. Incidunt dolores laborum tempora.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus quam cum eius architecto recusandae minima nemo nostrum blanditiis, dicta inventore, excepturi obcaecati ipsa minus. Sit, explicabo. Incidunt dolores laborum tempora.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus quam cum eius architecto recusandae minima nemo nostrum blanditiis, dicta inventore, excepturi obcaecati ipsa minus. Sit, explicabo. Incidunt dolores laborum tempora.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus quam cum eius architecto recusandae minima nemo nostrum blanditiis, dicta inventore, excepturi obcaecati ipsa minus. Sit, explicabo. Incidunt dolores laborum tempora.
+      <Button
+        variant="contained"
+        href="https://photo.noyeau.io/mo/request/A6doKILJP"
+        target="_blank"
+      >
+        Envoyez vos photos
+      </Button>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus quam cum
+      eius architecto recusandae minima nemo nostrum blanditiis, dicta
+      inventore, excepturi obcaecati ipsa minus. Sit, explicabo. Incidunt
+      dolores laborum tempora. Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Possimus quam cum eius architecto recusandae minima nemo
+      nostrum blanditiis, dicta inventore, excepturi obcaecati ipsa minus. Sit,
+      explicabo. Incidunt dolores laborum tempora. Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Possimus quam cum eius architecto recusandae
+      minima nemo nostrum blanditiis, dicta inventore, excepturi obcaecati ipsa
+      minus. Sit, explicabo. Incidunt dolores laborum tempora. Lorem ipsum dolor
+      sit amet consectetur adipisicing elit. Possimus quam cum eius architecto
+      recusandae minima nemo nostrum blanditiis, dicta inventore, excepturi
+      obcaecati ipsa minus. Sit, explicabo. Incidunt dolores laborum tempora.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus quam cum
+      eius architecto recusandae minima nemo nostrum blanditiis, dicta
+      inventore, excepturi obcaecati ipsa minus. Sit, explicabo. Incidunt
+      dolores laborum tempora. Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Possimus quam cum eius architecto recusandae minima nemo
+      nostrum blanditiis, dicta inventore, excepturi obcaecati ipsa minus. Sit,
+      explicabo. Incidunt dolores laborum tempora. Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Possimus quam cum eius architecto recusandae
+      minima nemo nostrum blanditiis, dicta inventore, excepturi obcaecati ipsa
+      minus. Sit, explicabo. Incidunt dolores laborum tempora. Lorem ipsum dolor
+      sit amet consectetur adipisicing elit. Possimus quam cum eius architecto
+      recusandae minima nemo nostrum blanditiis, dicta inventore, excepturi
+      obcaecati ipsa minus. Sit, explicabo. Incidunt dolores laborum tempora.
     </div>
   );
 }
