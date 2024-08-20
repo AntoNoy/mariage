7b3e69eedb7aad53e4b7550e22119b56b846868e
Antony <82900352+Antony-Wethenew@users.noreply.github.com>
2024-08-20 19:35:29 +0200

    fix cors

--- api/src/main.ts
+++ api/src/main.ts
@@ -2,9 +2,12 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors: {
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',  }});
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: '*',
+      methods: '*',
+    }
+  });
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
