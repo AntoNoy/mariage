6eae61ff2bbbb1e5b2af08f051838b0ffdcd8393
Antony <82900352+Antony-Wethenew@users.noreply.github.com>
2024-08-22 17:36:09 +0200

    manifest

--- app/pages/_document.tsx
+++ app/pages/_document.tsx
@@ -2,8 +2,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="fr">
+      <Head>
+      <link rel="manifest" href="/manifest.json" />
+
+      </Head>
       <body>
         <Main />
         <NextScript />
