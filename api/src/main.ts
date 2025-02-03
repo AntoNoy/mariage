f390446bba77cbf5b3f6433670eb43db0275b938
AntoNoy <anto75019@gmail.com>
2025-02-03 11:56:36 +0100

    fix cord

--- api/src/main.ts
+++ api/src/main.ts
@@ -8,19 +8,6 @@ async function bootstrap() {
       methods: '*',
     }
   });
-
-  app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://mariage.noyelle.eu');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-
-    if (req.method === 'OPTIONS') {
-      return res.sendStatus(204);
-    }
-
-    next();
-  });
-
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
